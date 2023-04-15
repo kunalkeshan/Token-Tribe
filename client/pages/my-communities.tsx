75277ea509766631a38521b6b1b2b1d0faeed10c
Kunal Keshan <kunalkeshan12@gmail.com>
2023-04-15 17:10:46 +0530

    chore: my community page

--- client/pages/my-communities.tsx
+++ client/pages/my-communities.tsx
@@ -1,7 +1,41 @@
+import PublicLayout from '@/layouts/PublicLayout';
 import React from 'react';
+import { GetStaticProps, InferGetStaticPropsType, NextPage } from 'next';
+import communities, { Communities } from '@/data/communities';
+import CommunityCard from '@/components/cards/CommunityCard';
 
-const MyCommunitiesPage = () => {
-	return <div>MyCommunitiesPage</div>;
+const MyCommunitiesPage: NextPage<
+	InferGetStaticPropsType<typeof getStaticProps>
+> = ({ myCommunities }) => {
+	return (
+		<PublicLayout>
+			<section className='w-full mt-8'>
+				<div className='max-w-[1240px] mx-auto'>
+					<h2 className='font-primary text-2xl font-semibold'>
+						My Communities
+					</h2>
+					<div className='w-full grid gap-4 grid-cols-1 sm:grid-cols-2 lg:grid-cols-3 mt-4'>
+						{myCommunities.map((community, index) => (
+							<CommunityCard {...community} key={index} />
+						))}
+					</div>
+				</div>
+			</section>
+		</PublicLayout>
+	);
 };
 
 export default MyCommunitiesPage;
+
+export const getStaticProps: GetStaticProps<{
+	myCommunities: Communities;
+}> = async () => {
+	const myCommunities = communities.filter(
+		(community) => community.myCommunity
+	);
+	return {
+		props: {
+			myCommunities,
+		},
+	};
+};
