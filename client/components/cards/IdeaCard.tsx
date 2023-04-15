bc850816f57f22b559ae622474aeba504eab5e91
Kunal Keshan <kunalkeshan12@gmail.com>
2023-04-15 20:07:17 +0530

    Feat/functionality (#28)
    * chore: replace poll with create idea form
    
    * chore: idea moddal styles
    
    * chore: support idea modal created

--- client/components/cards/IdeaCard.tsx
+++ client/components/cards/IdeaCard.tsx
@@ -1,17 +1,14 @@
-import React from 'react';
+import React, { useState } from 'react';
 import { Idea } from '@/data/communities';
 import { AiFillCalendar } from 'react-icons/ai';
 import { FaEthereum } from 'react-icons/fa';
 import { IoPeopleOutline } from 'react-icons/io5';
 import Countdown, { CountdownRendererFn } from 'react-countdown';
+import SupportIdeaModal from '../modals/SupportIdeaModal';
 
-const IdeaCard: React.FC<Idea> = ({
-	title,
-	target,
-	deadline,
-	raisedAmt,
-	contributors,
-}) => {
+const IdeaCard: React.FC<Idea> = (idea) => {
+	const [open, setOpen] = useState(false);
+	const { title, target, deadline, raisedAmt, contributors } = idea;
 	const infoItems = [
 		{
 			Icon: AiFillCalendar,
@@ -30,6 +27,10 @@ const IdeaCard: React.FC<Idea> = ({
 		},
 	];
 
+	const handleOpenSupportIdeaModal = () => {
+		setOpen(true);
+	};
+
 	const renderer: CountdownRendererFn = ({ days, completed }) => {
 		if (completed) {
 			return <></>;
@@ -43,24 +44,30 @@ const IdeaCard: React.FC<Idea> = ({
 	};
 
 	return (
-		<div className='block max-w-md p-6 border bg-[#222] rounded-lg shadow border-black text-secondary mb-4'>
-			<p className='font-primary font-bold text-lg'>{title}</p>
-			<div className='flex w-full items-center justify-between gap-4'>
-				<p>{target}</p>
-				<Countdown renderer={renderer} date={deadline} />
+		<>
+			<SupportIdeaModal open={open} setOpen={setOpen} idea={idea} />
+			<div
+				onClick={handleOpenSupportIdeaModal}
+				className='block max-w-md hover:scale-95 cursor-pointer p-6 border bg-[#222] hover:bg-[#373737] rounded-lg shadow border-black text-secondary mb-4 transition-all duration-300'
+			>
+				<p className='font-primary font-bold text-lg'>{title}</p>
+				<div className='flex w-full items-center justify-between gap-4'>
+					<p>{target}</p>
+					<Countdown renderer={renderer} date={deadline} />
+				</div>
+				<ul className='flex gap-4 items-center flex-wrap mt-4'>
+					{infoItems.map((item) => (
+						<li className='flex rounded-3xl hover:bg-opacity-40 transition-all duration-300 bg-tertiary gap-4 bg-opacity-20 items-center px-4 py-2'>
+							<item.Icon className='text-xl' />
+							<span className='text-left font-primary w-full text-xs'>
+								<p className='font-bold'>{item.title}</p>
+								<p className=''>{item.info}</p>
+							</span>
+						</li>
+					))}
+				</ul>
 			</div>
-			<ul className='flex gap-4 items-center flex-wrap mt-4'>
-				{infoItems.map((item) => (
-					<li className='flex rounded-3xl hover:bg-opacity-40 transition-all duration-300 bg-tertiary gap-4 bg-opacity-20 items-center px-4 py-2'>
-						<item.Icon className='text-xl' />
-						<span className='text-left font-primary w-full text-xs'>
-							<p className='font-bold'>{item.title}</p>
-							<p className=''>{item.info}</p>
-						</span>
-					</li>
-				))}
-			</ul>
-		</div>
+		</>
 	);
 };
 
