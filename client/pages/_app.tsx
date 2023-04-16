4039ad9bd318ebdf3e87feae6d073782ee2584f2
Kunal Keshan <kunalkeshan12@gmail.com>
2023-04-16 11:15:05 +0530

    no-font setup

--- client/pages/_app.tsx
+++ client/pages/_app.tsx
@@ -1,23 +1,23 @@
 import OgMetaTags from '@/components/resusable/OgMetaTags';
 import '@/styles/globals.css';
 import type { AppProps } from 'next/app';
-import { Syne, Nunito_Sans } from 'next/font/google';
+// import { Syne, Nunito_Sans } from 'next/font/google';
 import { CrowdFundingProvider } from '../context/CrowdFunding';
 import Head from 'next/head';
 
-const syne = Syne({
-	subsets: ['latin'],
-	weight: ['400', '500', '600', '700', '800'],
-	style: ['normal'],
-	variable: '--font-syne',
-});
+// const syne = Syne({
+// 	subsets: ['latin'],
+// 	weight: ['400', '500', '600', '700', '800'],
+// 	style: ['normal'],
+// 	variable: '--font-syne',
+// });
 
-const nunitoSans = Nunito_Sans({
-	subsets: ['latin'],
-	weight: ['200', '300', '400', '600', '700', '800', '900'],
-	style: ['italic', 'normal'],
-	variable: '--font-nunito-sans',
-});
+// const nunitoSans = Nunito_Sans({
+// 	subsets: ['latin'],
+// 	weight: ['200', '300', '400', '600', '700', '800', '900'],
+// 	style: ['italic', 'normal'],
+// 	variable: '--font-nunito-sans',
+// });
 
 export default function App({ Component, pageProps }: AppProps) {
 	return (
@@ -26,7 +26,7 @@ export default function App({ Component, pageProps }: AppProps) {
 				<OgMetaTags />
 			</Head>
 			<main
-				className={`${syne.variable} ${nunitoSans.variable} font-secondary bg-primary text-secondary w-full min-h-screen`}
+				className={`font-secondary bg-primary text-secondary w-full min-h-screen`}
 			>
 				<Component {...pageProps} />
 			</main>
